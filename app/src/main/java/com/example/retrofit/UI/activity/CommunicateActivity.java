e0d289f4153dd3059bd8c410a37995751e193133
Namosun <479304861@qq.com>
2020-05-09 15:06:28 +0800

    聊天界面样式设计

--- app/src/main/java/com/example/retrofit/UI/activity/CommunicateActivity.java
+++ app/src/main/java/com/example/retrofit/UI/activity/CommunicateActivity.java
@@ -2,11 +2,15 @@ package com.example.retrofit.UI.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.retrofit.R;
 
 public class CommunicateActivity extends AppCompatActivity {
+
 
 
 
@@ -15,5 +19,8 @@ public class CommunicateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communicate);
 
+    }
+    public  void  setBack(View view){
+        this.finish();
     }
 }
