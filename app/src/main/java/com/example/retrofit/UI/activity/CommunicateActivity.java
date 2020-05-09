448c9ee0fe93b12c7ecab33df46279f87e8978c1
Namosun <479304861@qq.com>
2020-05-09 14:31:09 +0800

    进入聊天界面

--- app/src/main/java/com/example/retrofit/UI/activity/CommunicateActivity.java
+++ app/src/main/java/com/example/retrofit/UI/activity/CommunicateActivity.java
@@ -14,5 +14,6 @@ public class CommunicateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communicate);
+
     }
 }
