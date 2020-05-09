1f5f37dfb1a5e94d1475662a5575c2cc257ae762
Namosun <479304861@qq.com>
2020-05-09 17:30:13 +0800

    聊天内容模拟

---
+++ app/src/main/java/com/example/retrofit/UI/adapter/MessageAdapter.java
@@ -0,0 +1,59 @@
+package com.example.retrofit.UI.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.retrofit.R;
+import com.example.retrofit.domain.MessageRespose;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.InnerHolder> {
+
+    private final List<MessageRespose.DataBean> mData;
+
+    public MessageAdapter(List<MessageRespose.DataBean> data){
+        this.mData=data;
+        System.out.println(mData);
+    }
+
+    @NonNull
+    @Override
+    public InnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=View.inflate(parent.getContext(), R.layout.item_message,null);
+        return new MessageAdapter.InnerHolder(view);
+      
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InnerHolder holder, int position) {
+        System.out.println(mData.get(position));
+        holder.setData(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mData!=null) {
+            return mData.size();
+        }
+        return 0;
+    }
+
+    public class InnerHolder extends RecyclerView.ViewHolder {
+        private TextView mTextView;
+        public InnerHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextView=itemView.findViewById(R.id.item_message_textView);
+        }
+
+        public void setData(MessageRespose.DataBean dataBean) {
+            mTextView.setText(dataBean.getContent());
+
+        }
+    }
+}
